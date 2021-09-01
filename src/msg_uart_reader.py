d204ed5cf8bbbeb5b3d7b212a044929c7b0e233c
arekosinski <ao@i-netpl.info>
2021-09-01 22:44:03 +0200

    uart reader refactor

--- src/msg_uart_reader.py
+++ src/msg_uart_reader.py
@@ -1,62 +1,79 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
 import serial
 import time
-import os
 import redis
+import logging
+import traceback
+import threading
+import queue
+import sys,os
+from lib.message_object import MessageObject
 
-from os.path import expanduser
+sys.path.append(os.path.abspath(os.path.dirname(__file__))+"/lib")
 
+import message_object
 
-def get_device_id(line):
-    return line[0:2]
+def redis_connection_open(host,port,database):
+    redis_conn = redis.Redis(host=host,port=port,db=database)
+    return redis_conn
 
-def get_measure_code(line):
-    return line[2:3]
-
-def get_measure_value(line):
-    return line[3:line.find('|',0)]
-
-def get_cycle_number(line):
-    return line[line.find('|',0)+1:len(line)].strip()
-
-def decode_msg(line):
-        msg = dict()
-        msg["timestamp"] = time.time()
-        msg["source_msg"] = line
-        msg['decoded'] = 1
-        try:
-                msg["device_id"] = get_device_id(line)
-                msg["measure_code"] = get_measure_code(line)
-                msg["measure_value"] = get_measure_value(line)
-                msg["cycle_number"] = get_cycle_number(line)
-                decoding_status=True
-        except:
-                decoding_status=False
-        return (msg,decoding_status)
-
-def redis_channel_open(host,port):
-    redis = redis.Redis(host=host,port=port)
-    return redis
-
-def redis_channel_close(redis):
+def redis_connection_close(redis):
     redis.close()
     return True
 
-def redis_write_message(msg,redis,redis_list):
-    # redis.lpush(database, "{msg}".format(msg))
-    return True
+def redis_write_message(msg,redis_conn,redis_list):
+    return redis_conn.lpush(redis_list, msg)
 
+def write_message_to_storage(processing_queue,redis_lists):
+    while True:
+        msg = processing_queue.get()
+        msg_received_count =+ 1
+
+        logging.debug("Processing {sensor_id}:{measurment_id}:{cycle}" \
+            .format(sensor_id=msg.get_device_id(),measurment_id=msg.get_measure_code(),cycle=msg.get_cycle_number())
+        )
+
+        # writing to redis
+        for r_list in redis_lists:
+            try:
+                redis_writing_status = redis_write_message(msg=msg.export_message(),redis_conn=redis_connection,redis_list=r_list)
+                if redis_writing_status == False:
+                    logging.error("Problem with writing message to redis database")
+            except Exception:
+                traceback.print_exc()
+                logging.error(traceback.print_exc())
+
+        # writing to file
+        try:
+            radio_log.write("{msgd}\n".format(msgd=msg.export_message()))
+
+            if msg_received_count % 100 == 0:
+                logging.info("Messages processed: {msg_c}".format(msg_c=msg_received_count))
+                radio_log.flush()
+
+        except Exception:
+            traceback.print_exc()
+            logging.error(traceback.print_exc())
+
+        processing_queue.task_done()
 
 ####
+
+logging.basicConfig(format='%(asctime)s %(levelname)s [%(threadName)10s]: %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.DEBUG)
+logging.info("Setting up env")
+
 uart_speed = 460800
 uart_port = '/dev/ttyAMA0'
 redis_server = "localhost"
 redis_port = 6379
-redis_list = "metrics"
-msg_logger_file = "{homedir}/radio_msg.log".format(homedir=expanduser("~"))
+redis_lists = ["metrics_pubsub","metrics_kinesis"]
+redis_database = 0
+msg_logger_file = "{homedir}/radio_msg.log".format(homedir=os.path.expanduser("~"))
+msg_received_count = 0
+
+messages_queue = queue.Queue()
 
 # CLI parser
 cli_parser = argparse.ArgumentParser(description='Script for getting messages from RPI UART and putting them into redis database')
@@ -65,40 +82,57 @@ cli_parser.add_argument('--uart-speed', action='store', type=int, required=False
 cli_parser.add_argument('--uart-port', action='store', type=str, required=False, default=uart_port,help="UART port in OS")
 cli_parser.add_argument('--redis-port', action='store', type=int, required=False, default=redis_port,help="Redis port to connect too")
 cli_parser.add_argument('--redis-server', action='store', type=str, required=False, default=redis_server,help="Redis server host address")
-cli_parser.add_argument('--redis-list', action='store', type=str, required=False, default=redis_list,help="On which Redis list I should work")
+cli_parser.add_argument('--redis-list', action='append', type=str, required=False, default=redis_lists,help="On which Redis list I should work")
+cli_parser.add_argument('--redis-database', action='store', type=int, required=False, default=redis_database,help="On which Redis list I should work")
 cli_parser.add_argument('--logger-file', action='store', type=str, required=False, default=msg_logger_file,help="Name of the file to store data (beside Redis)")
 
 cli_args = cli_parser.parse_args()
 
 if __name__ == '__main__':
-    # create serial handler
+
+    # create serial handler        
+    logging.info("Setting up serial connection")
     serial_proxy = serial.Serial(cli_args.uart_port, cli_args.uart_speed, timeout=1)
     serial_proxy.flush()
 
     # open redis communication
-    redis = redis_channel_open(hots=cli_args.redis_server,port=cli_args.redis_port,database=cli_args.redis_database)
+    logging.info("Setting up Redis connection")
+    redis_connection = redis_connection_open(host=cli_args.redis_server,port=cli_args.redis_port,database=cli_args.redis_database)
 
-    radio_log = open(cli_args.msg_logger_file,'a')
+    logging.info("Setting up local file for writing")
+    radio_log = open(cli_args.logger_file,'a')
 
+    logging.info("Setting up threads")
+    thread_msg_writer = threading.Thread(name="msgWriter", target=write_message_to_storage, args=(messages_queue,cli_args.redis_list))
+    thread_msg_writer.setDaemon(True)
+    thread_msg_writer.start()
+
+    logging.info("Reading messages from serial in loop")
     while True:
         if serial_proxy.in_waiting > 0:
             try:
                 line = serial_proxy.readline().decode('ascii').strip()
-                print("RAW MSG received: {msg}".format(msg=line))
-                if ('|' in line):
-                    (msg_decoded,decoding_status) = decode_msg(line)
-                    
-                    if decoding_status == False:
-                        print("ERR msg decoding erros")
-                    print("MSG decoded: {msgd}".format(msgd=msg_decoded))
-                    
-                    redis_write_message(msg=msg_decoded,redis=redis,redis_list=cli_args.redis_list)
+                logging.info("RAW MSG received: {msg}".format(msg=line))
 
-                    radio_log.write("{msgd}\n".format(msgd=msg_decoded))
-                    
-                    radio_log.flush()
-            except:
-                print("ERR handling message failed")
+                if MessageObject.detect_version(line):
+                    msg = MessageObject(line)
 
+                    # put message to queue for further processing
+                    if msg.validate() == True:
+                        messages_queue.put(msg)
+                        logging.debug("Message queued: {sensor_id}:{measurment_id}:{cycle}" \
+                            .format(sensor_id=msg.get_device_id(),measurment_id=msg.get_measure_code(),cycle=msg.get_cycle_number())
+                        )
+                    else:
+                        logging.warning("msg decoding erros")
+                        logging.debug("Parsing errors: {err}".format(err = ",".join(msg.get_validation_errors())))
+
+            except Exception:
+                traceback.print_exc()
+                logging.error(traceback.print_exc())
+
+    messages_queue.join()
+    redis_connection_close(redis_connection)
+    radio_log.flush()
     radio_log.close()
-    redis_close()
+    
