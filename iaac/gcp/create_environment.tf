6abd98514e2eb11941cd4fa23d13c3f83a843441
arek osinski <arek.osinski@i-netpl.info>
2021-10-21 23:23:46 +0200

    creating bg infra

--- iaac/gcp/create_environment.tf
+++ iaac/gcp/create_environment.tf
@@ -36,3 +36,73 @@ data "google_iam_policy" "pubsub_publisher" {
     members = [ format("serviceAccount:%s",google_service_account.pubsub_sa.email) ]
   }
 }
+
+resource "google_bigquery_dataset" "bq_iot_dataset" {
+  dataset_id                  = "iot_data"
+  friendly_name               = "iot_data"
+  location                    = "EU"
+}
+
+resource "google_bigquery_dataset_iam_member" "sa_grant_editor" {
+  dataset_id = google_bigquery_dataset.bq_iot_dataset.dataset_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${google_service_account.pubsub_sa.email}"
+}
+
+resource "google_bigquery_table" "iot_base_table" {
+  dataset_id = google_bigquery_dataset.bq_iot_dataset.dataset_id
+  table_id   = "iot_base_data"
+
+  time_partitioning {
+    type = "DAY"
+    require_partition_filter = true
+    field = "event_timestamp"
+  }
+
+  clustering = ["device_id","measure_name"]
+
+  schema = <<EOF
+  [{
+    "name": "event_timestamp",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE",
+    "description": "event generation time"
+  },
+  {
+    "name": "device_id",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "Device identification number"
+  },
+  {
+    "name": "measure_code",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "Measure code - short version"
+  },
+  {
+    "name": "measure_name",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "Measure code - long version"
+  },
+  {
+    "name": "measure_value",
+    "type": "NUMERIC",
+    "mode": "NULLABLE",
+    "description": "Measurment taken by device for measure code"
+  },
+  {
+    "name": "cycle_number",
+    "type": "INTEGER",
+    "mode": "NULLABLE",
+    "description": "Measurment taken by device for measure code"
+  },
+  {
+    "name": "version",
+    "type": "INTEGER",
+    "mode": "NULLABLE",
+    "description": "Measurment taken by device for measure code"
+  }]
+EOF
+}
